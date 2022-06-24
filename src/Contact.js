f1c707a7ec4b87c6d9e7989b1ef9f07789c1de83
Lastkingjayjay <juniorjosephr@gmail.com>
2022-06-24 16:53:27 -0400

    contact pagr changes

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Contacts</h1>
+      <div className="contact">Phone number</div>
+      <div className="container1"></div>
+    </>
+  );
 };
 
 export default Contact;
